4785847b3fb4da054f34bacb1ccd06d59460a27f
John Balo <john_balo@hotmail.com>
2018-03-22 21:10:49 +0000

    recipe app state refactor

--- ReactApp/recipe-app/src/RecipeApp.js
+++ ReactApp/recipe-app/src/RecipeApp.js
@@ -4,15 +4,37 @@ import RecipeList from "./RecipeList";
 import Navbar from "./Nav.js";
 
 class RecipeApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [{
+        id: 1,
+        title: "Pasta",
+        ingredients: ["flour", "water"],
+        instructions: "Mix ingredients",
+        img: "pasta.jpg"
+      },{
+        id: 2,
+        title: "Steak",
+        ingredients: ["Meat", "Salt"],
+        instructions: "Salt the steak and cook",
+        img: "steak.jpg"
+      },{
+        id: 3,
+        title: "Burger",
+        ingredients: ["Buns", "Patty"],
+        instructions: "Get the patty in the buns",
+        img: "burger.jpg"
+      }],
+      nextRecipeID: 3,
+    };
+  }
   render() {
     return (
       <div className="App"> 
         <Navbar />
         <RecipeList 
-          title="Pasta" 
-          ingredients={["flour", "water"]}
-          instructions="Mix ingredients"
-          img="pasta.jpg"
+          recipes = {this.state.recipes}
         />
       </div>
     );
--- ReactApp/recipe-app/src/RecipeList.js
+++ ReactApp/recipe-app/src/RecipeList.js
@@ -3,27 +3,9 @@ import Recipe from "./Recipe";
 import "./RecipeList.css";
 
 class RecipeList extends Component {
-      static defaultProps = {
-          recipes: [{
-            title: "Pasta",
-            ingredients: ["flour", "water"],
-            instructions: "Mix ingredients",
-            img: "pasta.jpg"
-          },{
-            title: "Steak",
-            ingredients: ["Meat", "Salt"],
-            instructions: "Salt the steak and cook",
-            img: "steak.jpg"
-          },{
-            title: "Burger",
-            ingredients: ["Buns", "Patty"],
-            instructions: "Get the patty in the buns",
-            img: "burger.jpg"
-          }]
-      }
     render() {
-        const recipes = this.props.recipes.map((r,index) => (
-          <Recipe key={index} {...r} />
+        const recipes = this.props.recipes.map(r => (
+          <Recipe key={r.id} {...r} />
         ));
         
         return (
